51cd64e6269d8c9199084cd191370758780b6ee1
Shan Eapen Koshy <shaneapen@gmail.com>
2018-07-30 21:57:19 +0530

    contact form fix

--- assets/js/contact-form-script.js
+++ assets/js/contact-form-script.js
@@ -19,7 +19,7 @@ function submitForm(){
 
     $.ajax({
         type: "POST",
-        url: "../php/form-process.php",
+        url: "../../contact-us.php",
         data: "name=" + name + "&email=" + email + "&message=" + message,
         success : function(text){
             if (text == "success"){
--- contact-us.php
+++ contact-us.php
@@ -1,6 +1,5 @@
-
 <?php
-if (isset ( $_POST['submit'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
   $name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
@@ -10,6 +9,6 @@ if (isset ( $_POST['submit'])) {
   $subject = "AKPESSC Contact Form";
   $mailheader = "From: $email \r\n";
   mail($recipient, $subject, $formcontent, $mailheader) or die("Error sending email!");
-  echo "Thank You!";
+  echo "success";
 }
 ?>
